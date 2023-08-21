6710e152e108d0ee076974444b98a6608c961c17
myominhtoo <myominhtoo2003@gmail.com>
2023-08-21 15:45:19 +0630

    feat(components) : added primary button component

--- note-web/src/App.tsx
+++ note-web/src/App.tsx
@@ -1,5 +1,21 @@
 import {msg} from 'pages'
+import {Button} from 'components/buttons'
 
-const App = () => <p>{msg}</p>
+const App = () => {
+  return (
+    <div
+      style={{
+        width: '300px',
+      }}
+    >
+      <Button onClick={() => console.log('pressed button')} text={'Login'} />
+      <Button
+        onClick={() => console.log('pressed button')}
+        style={{backgroundColor: 'red'}}
+        text={'Login'}
+      />
+    </div>
+  )
+}
 
 export default App
